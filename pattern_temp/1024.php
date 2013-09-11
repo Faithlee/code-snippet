85bc54d1353b22157417a7e6983866132016e745
faithlee <lijiabin098@126.cm>
2013-09-11 23:46:02 +0800

    add new file

--- pattern_temp/1024.php
+++ pattern_temp/1024.php
@@ -1,56 +1,26 @@
 <?php
 
-function __autoload($className) 
-{
-    $file = '../' . $className . '.php';
+//面向对象与面向过程的区别：职责分配不同，过程化忙于处理细节问题
+//如果多种文件格式，则需要处理多次
 
-    try {
-        if (!file_exists($file)) {
-            throw new Exception("the file not exists!\n");
-        }
-        
-        require $file;
-    } catch (Exception $e) {
-        echo $e->getMessage();
+function readParams($source) {
+    $parma = array();
+
+    if (preg_match('/\.xml$/', $source)) {
+        //读取方式1
+    } else {
+        //读取方式2
     }
+
+    return $param;
 }
 
-$product = new CDProduct('cards', 30, 'green');
-
-$reflectObj = new ReflectionClass('CDProduct');
-$method = $reflectObj->getMethod('getSummaryLine');
-
-//var_dump($method);
-//获取文件路径
-$file = $method->getFileName();
-
-//获取文件每行的内容
-$lineInfo = file($file);
-
-//获取当前方法的开始行
-$startLine = $method->getStartLine();
-
-//获取当前方法的结束行
-$endLine = $method->getEndLine();
-
-$content = array_slice($lineInfo, $startLine - 1, $endLine);
-
-//获取类的参数：
-$method2 = $reflectObj->getMethod('__construct');
-$paramObj = $method2->getParameters();
-
-//getName()方法、getClass()方法
-
-$param1 = $paramObj[0];
-
-$name = $param1->getName();
-
-$class = $param1->getClass();
-
-print_r($class);
-
-
-
-
+function writeParam($param, $source) {
+    if (preg_match('/\.xml$/i', $source)) {
+        //写入方式1
+    } else {
+        //写入方式2
+    }
+}
 
 ?>
