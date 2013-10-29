b090247acf4831e1378b7f211068ebc5e775ce5c
faithlee <lijiabin098@126.cm>
2013-10-30 06:23:20 +0800

    temp remove

--- Node.js/mongoose/connect-test.js
+++
@@ -1,8 +0,0 @@
-var db = require('mongoose').connection;
-
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', function callback(){
-	console.log('hello')	
-});
-
-console.log('Server has started!');
--- Node.js/mongoose/mongo.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-
-var db = mongoose.connect('mongodb://127.0.0.1:27017/test');
-
-mongoose.connection.once('connected', function(){
-	console.log('Connected to database!');
-});
