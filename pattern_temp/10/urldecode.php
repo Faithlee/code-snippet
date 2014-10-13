388e23e0bf4012d5e0583626197ed060e20ce824
faithlee <lijiabin098@126.com>
2014-10-14 00:16:24 +0800

    set baseUrl

---
+++ pattern_temp/10/urldecode.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @FileName: urldecode.php
+ * @Desc: 
+ * @Author: Faithlee098
+ * @Mail: lijiabin098@126.com 
+ * @CTime: Mon 13 Oct 2014 11:43:57 PM CST
+ */
+
+#将加号转换成空格
+$str = 'green+and+red';
+echo urldecode($str);
--- zend/library/Controller/Request/Http.php
+++ zend/library/Controller/Request/Http.php
@@ -82,17 +82,131 @@ class Zend_Controller_Request_Http extends Zend_Controller_Request_Abstract {
 			$filename = (isset($_SERVER['SCRIPT_FILENAME'])) ? basename($_SERVER['SCRIPT_FILENAME']) : '';
 			if (isset($_SERVER['SCRIPT_NAME']) && basename($_SERVER['SCRIPT_NAME']) === $filename) {
 				$baseUrl = $_SERVER['SCRIPT_NAME'];
-			} else if () {
-			
+			} else if (isset($_SERVER['PHP_SELF']) && basename($_SERVER['PHP_SELF']) == $filename) {
+				$baseUrl = $_SERVER['PHP_SELF'];
+			} else if (isset($_SERVER['ORIG_SCRIPT_NAME']) && basename($_SERVER['ORIG_SCRIPT_NAME']) === $filename) {
+				$baseUrl = $_SERVER['ORIG_SCRIPT_NAME'];
 			}
 
+			$path = isset($_SERVER['PHP_SELF']) ? $_SERVER['PHP_SELF'] : '';
+			$file = isset($_SERVER['SCRIPT_FILENAME']) ? $_SERVER['SCRIPT_FILENAME'] : '';
+			
+			$segs = explode('/', trim($file, '/'));
+			$segs = array_reverse($segs);
+			$index = 0;
+			$last = count($segs);
+			$baseUrl = '';
 
+			do {
+				$baseUrl = '/' . $segs[$index] . $baseUrl;
+				$index++;
+			} while ($last > $index && (false !== ($pos = strpos($path, $baseUrl))) && 0 !== $pos); 
+		}
+		
+		#uri统一资源标识 如public/dwz 或 public/dwz/index.php
+		#requestUri：/dwz/public/index.php?c=IndexController
+		#则baseUrl: /dwz/public/index.php
+		$requestUri = $this->getRequestUri();
+		if (0 === strpos($requestUri, $baseUrl)) {
+			$this->_baseUrl = $baseUrl;
+
+			return $this;
 		}
 
+		#requestUri:/dwz/public/index/demo
+		#$baseUrl: /dwz/public/index.php
+		if (0 === strpos($requestUri, dirname($baseUrl))) {
+			$this->_baseUrl = rtrim(dirname($baseUrl), '/');
 
+			return $this;
+		}
+
+		#通过?传递参数的请求		
+		$truncatedReqeustUri = $requestUri;
+		if ($pos = strpos($requestUri, '?') !== false) {
+			$truncatedReqeustUri = substr($requestUri, 0, $pos);
+		}
+
+		$basename = basename($baseUrl);
+		if (empty($basename) || !strpos($truncatedReqeustUri, $basename)) {
+			$this->_baseUrl = '';
+
+			return $this;
+		}
+
+		#use mod_rewrite strip the script fileame out of baseUrl; 不明白
+		if (strlen($requestUri) >= strlen($baseUrl) && ((false !== ($pos = strpos($requestUri, $baseUrl))) && ($pos !== 0))) {
+			$baseUrl = substr($requestUri, 0, $pos + strlen($baseUrl));
+		}
+
+		$this->_baseUrl = rtrim($baseUrl, '/');
+
+		return $this;
 	}
 	
 	/*}}}*/
+	/*{{{public function getBaseUrl()*/
+
+	#retrieve baseUrl
+	public function getBaseUrl()
+	{
+		if (null === $this->_baseUrl) {
+			$this->setBaseUrl();
+		}
+
+		return urldecode($this->_baseUrl);
+	}
+
+	/*}}}*/
+	/*{{{public function setBasePath()*/
+
+	public function setBasePath($basePath = null)
+	{
+		if ($basePath === null)	{
+			$filename = isset($_SERVER['SCRIPT_FILENAME']) ? basename($_SERVER['SCRIPT_FILENAME']) : '';
+						
+			$baseUrl = $this->getBaseUrl();
+			if (empty($baseUrl)) {
+				$this->_basePath = '';
+				return $this;
+			}
+
+			if (basename($baseUrl) === $filename) {
+				$basePath = dirname($baseUrl);
+			} else {
+				$basePath = $baseUrl;
+			}
+		}
+
+		if (substr(PHP_OS, 0, 3) === 'WIN')	{
+			$basePath = str_replace('\\', '/', $basePath);
+		}
+
+		$this->_basePath = rtrim($basePath, '/');
+
+		return $this;
+	}
+	
+	/*}}}*/
+	/*{{{public function getBasePath()*/
+
+	#retrieve
+	public function getBasePath()
+	{
+		if (null === $this->_basePath) {
+			$this->setBasePath();
+		}
+
+		return $this->_basePath;
+	}
+	
+	/*}}}*/
+	/*{{{*/
+
+		
+	
+	/*}}}*/
+
 
 
 	/*{{{public function getServer()*/
