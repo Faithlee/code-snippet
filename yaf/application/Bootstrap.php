a18501b289fcaa206a64309dfb16e865a6da4b0c
faithlee <lijiabin098@126.com>
2014-10-16 23:58:37 +0800

    yaf application practise

--- yaf/application/Bootstrap.php
+++ yaf/application/Bootstrap.php
@@ -1,15 +1,41 @@
 <?php
 /**
  * @FileName: Bootstrap.php
- * @Desc: 
+ * @Desc: bootstrap引导程序，提供了一个全局配置入口
  * @Author: Faithlee098
  * @Mail: lijiabin098@126.com 
  * @CTime: Thu 16 Oct 2014 12:30:43 AM CST
  */
 
 class Bootstrap extends Yaf_Bootstrap_Abstract {
-	public function _init()
+	/*{{{public function _initPlugin()*/
+	
+	#注册一个插件
+	public function _initPlugin(Yaf_Dispatcher $dispatcher)
+	{
+		$plugin = new UserPlugin();
+		$dispatcher->registerPlugin($plugin);
+	}
+	
+	/*}}}*/
+	/*{{{public function _initConfig()*/
+
+	public function _initConfig()
+	{
+			
+	}
+
+	/*}}}*/
+	/*{{{public function _initView()*/
+
+	public function _initView(Yaf_Dispatcher $dispatcher)
 	{
 	
 	}
+
+	/*}}}*/
+
+
+
+
 }
