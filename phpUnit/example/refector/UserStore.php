1f8a381de6914eb50fd74049f520eb58d9cd4f7f
faithlee <lijiabin098@126.com>
2014-10-28 01:41:20 +0800

    code refector test

---
+++ phpUnit/example/refector/UserStore.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @FileName: UserStore.php
+ * @Desc: 代码重构：将成员属性储存数组改为储存对象
+ * @Author: Faithlee098
+ * @Mail: lijiabin098@126.com 
+ * @CTime: Tue 28 Oct 2014 01:17:56 AM CST
+ */
+
+require_once 'User.php';
+
+class UserStore {
+	private $users;
+
+	public function addUser($name, $mail, $passwd)
+	{
+		if (isset($this->users[$name]))	{
+			throw new Exception("User {$mail} already exists!");
+		}
+
+		$this->users[$mail] = new User($name, $mail, $passwd);
+
+		return true;
+	}
+
+	public function notifyPasswordFailure($mail, $passwd)
+	{
+		if (isset($this->users[$mail])) {
+			$this->users[$mail]->failed(time());
+		}
+	}
+
+
+	public function getUser($mail)
+	{
+		if (isset($this->users[$mail])) {
+			return $this->users[$mail];
+		}
+
+		return null;
+	}
+}
