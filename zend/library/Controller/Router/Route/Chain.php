a9432679fee980bbe0c0ea436ad4916dcfbca9c8
Faithlee <lijiabin098@126.com>
2014-11-18 00:47:10 +0800

    zend route module

--- zend/library/Controller/Router/Route/Chain.php
+++ zend/library/Controller/Router/Route/Chain.php
@@ -45,22 +45,86 @@ class Zend_Controller_Router_Route_Chain extends Zend_Controller_Router_Route_Ab
 		$path = trim($request->getPathInfo(), '/');
 		$subPath = $path;
 		$values = array();
+		foreach ($this->_routes as $key => $route) {
+			if ($key > 0 && $matchedPath !== null && $subPath !== '' && $subPath !== false) {
+				$separator = substr($subPath, 0, strlen($this->_separators[$key]));
+
+				if ($separator !== $this->_separator[$key]) {
+					return false;	
+				}
+
+				$subPath = substr($subPath, strlen($separator));
+			}
+
+			if (!method_exists($route, 'getVersion') || $route->getVersion() == 1) {
+				$match = $subPath;	
+			} else {
+				$request->setPathInfo($subPath);
+				$match = $request;
+			}
+
+			$res = $route->match($match, true);
+			if ($res === false) {
+				return false;
+			}
+
+			$matchedPath = $route->getMatchedPath();
+
+			if ($matchedPath !== null) {
+				$subPath = substr($subPath, strlen($matchedPath));
+				$separator = substr($substr, 0, strlen($this->_separator[$key]));
+			}
+
+			$values = $res + $values;
+		}
+
+		$request->setPathInfo($path);
+		if ($subPath !== '' && $subPath !== false) {
+			return false;
+		}
+
+		return $values;
 	}
 
 	/*}}}*/
 	/*{{{public function assemble()*/
 
-	public function assemble()
+	#assembles a url defined by this route
+	public function assemble($data = array(), $reset = false, $encode = false)
 	{
-	
+		$value = '';
+		$numRoutes = count($this->_routes);
+
+		foreach ($this->_routes as $key => $route) {
+			if ($key > 0) {
+				$value .= $this->_separators[$key];
+			}
+
+			$value .= $route->assemble($data, $reset, $encode, (($numRoutes - 1) > $key));
+
+			if (method_exists($route, 'getVariables')) {
+				$variables = $route->getVariables();
+				foreach ($variables as $variable) {
+					$data[$variable] = null;
+				}
+			}
+		}
+
+		return $values;
 	}
 	
 	/*}}}*/
 	/*{{{public function setRequest()*/
 
-	public function setRequest()
+	#set the request object for this and the child routes
+	public function setRequest(Zend_Controller_Request_Abstract $request = null)
 	{
-	
+		$this->_request = $request;	
+		foreach ($this->_routes as $routes) {
+			if (method_exists($route, 'setReqeust')) {
+				$route->setRequest($request);
+			}
+		}
 	}
 	
 	/*}}}*/
--- zend/library/Controller/Router/Route/Module.php
+++ zend/library/Controller/Router/Route/Module.php
@@ -7,6 +7,107 @@
  * @CTime: Wed 08 Oct 2014 05:04:35 AM CST
  */
 
+require_once 'Zend/Controller/Router/Route/Abstract.php';
+
 class Zend_Contrller_Router_Route_Module extends Zend_Controller_Router_Route_Module {
-	//todo 
+	/*{{{members*/
+	
+	/**
+	 * URI delimiter
+	 */
+	const URI_DELIMITER = '/';
+		
+	/**
+	 * default values for the route(moduel, controller, action, params)
+	 */
+	protected $_defaults;
+
+	protected $_values = array();
+	protected $_moduleValid = false;
+	protected $_keysSet = false;
+	
+	/**
+	 * array keys to use for module, controller, and action
+	 */
+	protected $_moduleKey = 'module';
+	protected $_controllerKey = 'controller';
+	portected $_actionKey = 'action';
+
+	/**
+	 * @var Zend_Controller_Dispatcher_Interface
+	 */
+	protected $_dispatcher;
+
+	/**
+	 * @var Zend_Controller_Reqeust_Abstract
+	 */
+	protected $_request;
+
+	/*}}}*/
+	/*{{{functions*/
+	/*{{{public function getVersion()*/
+
+
+	public function getVersion()
+	{
+		return 1;	
+	}
+
+	/*}}}*/
+	/*{{{public static function getInstance()*/
+	
+	public static function getInstance(Zend_Config $config)
+	{
+		$frontController = Zend_Controller_Front::getInstance();
+
+		$defs = ($config->defaults instanceof Zend_Config) ? $config->defaults->toArray() : array();
+		$dispatcher = $frontController->getDispatcher();
+		$request = $frontController->getReqeust();
+
+		return new self($defs, $dispatcher, $request);
+	}
+	
+	/*}}}*/
+	/*{{{public function __construct()*/
+
+	public function __construct(array $defaults = array(),
+			Zend_Controller_Dispatcher_Interface $dispatcher = null,
+			Zend_Controller_Request_Abstract $request = null
+		)
+	{
+		$this->_defaults = $defaults;
+
+		if (isset($request)) {
+			$this->_request = $request;
+		}
+		
+		if (isset($request)) {
+			$this->_dispatcher = $dispatcher;
+		}
+	}
+	
+	/*}}}*/
+	/*{{{protected function _setRequestKeys()*/
+	
+	#set request keys on values in request object
+	protected function _setRequestKeys()
+	{
+		if (null !== $this->_request) {
+			$this->_moduleKey = $this->_request->getModuleKey();
+			$this->_controllerKey = $this->_request->getControllerKey();
+			$this->_actionKey = $this->_request->getActionKey();
+		}
+
+		if (null !== $this->_dispatcher) {
+			$this->_defaults += array(
+				$this->_controllerKey => $this->_dispatcher->getDefaultControllerName(),
+				$this->_actionKey => $this->_dispacther->getDefaultAction(),
+				$this->_moduleKey => $this->_dispatcher->getDefaultModule()
+			);
+		}
+	}
+	
+	
+	/*}}}*/
+	/*}}}*/
 }
