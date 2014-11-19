c0acc297965e9bf03986368f64f8b3125e80591e
Faithlee <lijiabin098@126.com>
2014-11-19 23:59:48 +0800

    yaf

---
+++ yaf/application/library/Foo/Bar.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @FileName: Bar.php
+ * @Desc: 
+ * @Author: Faithlee
+ * @Mail: lijiabin098@126.com 
+ * @CTime: Wed 19 Nov 2014 11:17:53 PM CST
+ */
+class Foo_Bar {
+	
+	public function __construct()
+	{
+		
+		echo __METHOD__ . '在library库中定义一些本地类：' . "<br/>";
+	}
+}
